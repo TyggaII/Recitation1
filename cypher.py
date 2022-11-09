a0af570c72783201d3d14e3cda77bf4b22ab19be
TyggaII <99297527+TyggaII@users.noreply.github.com>
2022-11-08 23:11:35 -0500

    Update cypher.py

--- cypher.py
+++ cypher.py
@@ -1,9 +1,10 @@
-# Division of PEMaCS
-# CSCI-121 Elements of Computer Programming II
-# Recitation 1 - Encryption with a password
+# *******************************************************
+# CSI-121 Elements of Computer Programming II
+# Caesar Cypher
 # *******************************************************
 
 import string
+import math
 
 alphabet = string.printable
 ordinal_value = {ch: i for i, ch in enumerate(alphabet)}
@@ -11,9 +12,27 @@ ordinal_value = {ch: i for i, ch in enumerate(alphabet)}
 
 def encrypt(message, password):
     encrypted_message = ''
-    pass  # TODO replace this line with your code.
+    password = math.ceil(len(message)/len(password))*password
+    for index, ch in enumerate(message):
+        pass_ch = password[index]
+        key = ordinal_value[pass_ch]
+        ord_of_ch = ordinal_value[ch]
+        shifted_ord_of_ch = (ord_of_ch + key) % len(alphabet)
+        encrypted_ch = alphabet[shifted_ord_of_ch]
+        encrypted_message += encrypted_ch
+    return encrypted_message
 
 
 def decrypt(message, password):
     decrypted_message = ''
-    pass  # TODO replace this line with your code.
+    password = math.ceil(len(message) / len(password)) * password
+    for index, ch in enumerate(message):
+        pass_ch = password[index]
+        key = ordinal_value[pass_ch]
+        ord_of_ch = ordinal_value[ch]
+        shifted_ord_of_ch = (ord_of_ch - key) % len(alphabet)
+        decrypted_ch = alphabet[shifted_ord_of_ch]
+        decrypted_message += decrypted_ch
+    return decrypted_message
+
+
